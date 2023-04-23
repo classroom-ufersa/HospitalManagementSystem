1314f3200f9c11d3c81e8ca440f57643009e287a
JhoanDev <jhoandev.log@gmail.com>
2023-04-23 06:50:35 -0300

    feat: add main.c

---
+++ Hospital.c/src/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int controle = 0;
+    printf("+----------------------------------------------------------------+\n"
+           "|        Bem-vindo ao Programa de Gerenciamento Hospitalar       |\n"
+           "|                                                                |\n"
+           "|           Autores: Jhoan Fernandes    Felipe Tomaz             |\n"
+           "+----------------------------------------------------------------+\n\n");
+    while (controle != 8) // vai repetir ate o usuario digitar 8
+    {
+        printf("Menu:\n");
+        printf("========================================\n");
+        printf("[1] Cadastrar paciente\n");
+        printf("[2] Excluir paciente\n");
+        printf("[3] Listar pacientes\n");
+        printf("[4] Buscar paciente\n");
+        printf("[5] Editar cadastro de paciente\n");
+        printf("[6] Consultar vagar disponiveis de leitos no hospital\n");
+        printf("[7] Consultar quantidade de pacientes\n");
+        printf("[8] Sair\n");
+        printf("========================================\n");
+        printf("Digite o numero da opcao desejada: ");
+        scanf("%d", &controle);
+        system("cls");
+        switch (controle)
+        {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        default:
+            printf("opcao invalida!");
+            break;
+        }
+    }
+    return 0;
+}
