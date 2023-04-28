12124fd69191820c4e93e425d3981510cb30e7ab
lftmaz <luizfelipe123924@gmail.com>
2023-04-28 14:53:11 -0300

    fix: criando tad pacientes.py

--- Hospital.py/src/hospital.py
+++ Hospital.py/src/hospital.py
@@ -1,21 +1,4 @@
-class Document:
-    def __init__(self, cpf="", rg=""):
-        self.cpf = cpf
-        self.rg = rg
-
-class Paciente:
-    def __init__(self, nome="", documento=None, enfermidade="", internado=0, receita=""):
-        self.nome = nome
-        self.documento = documento if documento is not None else Document()
-        self.enfermidade = enfermidade
-        self.internado = internado
-        self.receita = receita
-
-class ListaPacientes:
-    def __init__(self, paciente=Paciente()):
-        self.paciente = paciente
-        self.next = None
-        self.prev = None
+from pacientes import *
 
 class Hospital:
     def __init__(self, codigo=77, leitos=25, localizacao="", nome="", lista=ListaPacientes(), numpacientes = 0):
