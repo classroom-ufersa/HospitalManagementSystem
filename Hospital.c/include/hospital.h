aa117a342ed14450a36802407eea084a8ae19e86
JhoanDev <jhoandev.log@gmail.com>
2023-04-29 03:15:08 -0300

    feat: verifications

--- Hospital.c/include/hospital.h
+++ Hospital.c/include/hospital.h
@@ -11,8 +11,6 @@ Hospital *cadastra_paciente(Hospital *h, int *qnt);
 // cria o hospital
 Hospital *hospital_cria(void);
 
-
-
 // le o arquivo e armazena na lista encadeada dupla
 void ler_arquivo(Hospital *h, char *caminho, int *qnt);
 
